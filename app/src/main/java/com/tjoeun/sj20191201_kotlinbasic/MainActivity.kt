a455af6b6ab77aba5a72e07eab2289e34c3514b8
jun <vagrantsj@gmail.com>
2019-12-01 10:25:57 +0900

    온클릭 접근까지

--- app/src/main/java/com/tjoeun/sj20191201_kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/tjoeun/sj20191201_kotlinbasic/MainActivity.kt
@@ -2,11 +2,18 @@ package com.tjoeun.sj20191201_kotlinbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)//XML 불러오는 역할
+//        (okBtn) 버튼이 눌리면
+//        EditTxt (inputEdt)에 적힌 값을
+//        TextView의 (resultTxt)내용으로 반영
+        okBtn.setOnClickListener {
+
+        }
     }
 }
