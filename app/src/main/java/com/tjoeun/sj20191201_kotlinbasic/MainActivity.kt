2410f7d7e791aebcf000efe38155c711a3b68204
jun <vagrantsj@gmail.com>
2019-12-01 10:29:02 +0900

    입력값을 받아서 텍스트 뷰에 반영

--- app/src/main/java/com/tjoeun/sj20191201_kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/tjoeun/sj20191201_kotlinbasic/MainActivity.kt
@@ -10,9 +10,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)//XML 불러오는 역할
 //        (okBtn) 버튼이 눌리면
-//        EditTxt (inputEdt)에 적힌 값을
+//        EditTxt (inputEdt)에 적힌 값(text)을 받아서
 //        TextView의 (resultTxt)내용으로 반영
+
         okBtn.setOnClickListener {
+            inputEdt.text.toString()
+            var inputString = inputEdt.text.toString()
+            resultTxt.text = inputString
 
         }
     }
