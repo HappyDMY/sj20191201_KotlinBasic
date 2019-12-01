77c53d13cd3b916a9bd275021ea63c86fececebe
jun <vagrantsj@gmail.com>
2019-12-01 11:27:10 +0900

    로그찍기

--- app/src/main/java/com/tjoeun/sj20191201_kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/tjoeun/sj20191201_kotlinbasic/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.sj20191201_kotlinbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,16 +15,22 @@ class MainActivity : AppCompatActivity() {
 //        TextView의 (resultTxt)내용으로 반영
 
         okBtn.setOnClickListener {
-            inputEdt.text.toString()
-            var inputString = inputEdt.text // get 내용물 받아오기.
+            var inputString = inputEdt.text.toString() // get 내용물 받아오기.
             resultTxt.text = inputString // set 내용물 설정하기.
+
+//            확인버튼이 눌렸는지 로크
+            Log.d("확인버튼 클릭", inputString)
+
         }
 
         okBtn.setOnLongClickListener {
             resultTxt.text = "초기상태"
+
+            Log.d("확인버튼 클릭","텍스트뷰 초기화")
             return@setOnLongClickListener true //롱클릭이 됐을 때 온클릭 무시 여부
         }
 
+        var int:Int
 
     }
 }
